df3b3c6b3522ea387f618feab1ab9b4d248cbf6d
Javier Ayuso <javier.ayuso@gmail.com>
2020-12-18 09:34:10 +0100

    completed stack

--- README.md
+++ README.md
@@ -12,3 +12,23 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
+## Blog
+
+Rapid and flexible Infrastructure as Code using the AWS CDK with AWS Solutions Constructs
+https://aws.amazon.com/blogs/devops/rapid-flexible-infrastructure-with-solutions-constructs-cdk/
+
+## Feed the Stream
+
+```
+aws kinesis put-records \
+--stream-name StreamIngestionStack-KinesisLambdaConstructKinesisStreamXXXXXXXX-XXXXXXXXXXXX \
+--cli-binary-format raw-in-base64-out \
+--records \
+PartitionKey=1301,'Data=15.4|2020-08-22T01:16:36+00:00' \
+PartitionKey=1503,'Data=39.1|2020-08-22T01:08:15+00:00'
+```
+
+
+
+
--- lib/stream-ingestion-cdk-stack.ts
+++ lib/stream-ingestion-cdk-stack.ts
@@ -1,9 +1,65 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import { KinesisStreamsToLambda } from "@aws-solutions-constructs/aws-kinesisstreams-lambda";
 
-export class StreamIngestionCdkStack extends cdk.Stack {
+import * as ddb from "@aws-cdk/aws-dynamodb";
+import { LambdaToDynamoDB } from "@aws-solutions-constructs/aws-lambda-dynamodb";
+
+export class StreamIngestionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const kinesisLambda = new KinesisStreamsToLambda(
+      this,
+      "KinesisLambdaConstruct",
+      {
+        lambdaFunctionProps: {
+          // Where the CDK can find the lambda function code
+          runtime: lambda.Runtime.NODEJS_10_X,
+          handler: "lambdaFunction.handler",
+          code: lambda.Code.fromAsset("lambda"),
+        },
+      }
+    );
+
+    // Next Solutions Construct goes here
+    // Define the primary key for the new DynamoDB table
+    const primaryKeyAttribute: ddb.Attribute = {
+      name: "partitionKey",
+      type: ddb.AttributeType.STRING,
+    };
+
+    // Define the sort key for the new DynamoDB table
+    const sortKeyAttribute: ddb.Attribute = {
+      name: "timestamp",
+      type: ddb.AttributeType.STRING,
+    };
+
+    const lambdaDynamoDB = new LambdaToDynamoDB(
+      this,
+      "LambdaDynamodbConstruct",
+      {
+        // Tell construct to use the Lambda function in
+        // the first construct rather than deploy a new one
+        existingLambdaObj: kinesisLambda.lambdaFunction,
+        tablePermissions: "Write",
+        dynamoTableProps: {
+          partitionKey: primaryKeyAttribute,
+          sortKey: sortKeyAttribute,
+          billingMode: ddb.BillingMode.PROVISIONED,
+          removalPolicy: cdk.RemovalPolicy.DESTROY
+        },
+      }
+    );
+
+    // Add autoscaling
+    const readScaling = lambdaDynamoDB.dynamoTable.autoScaleReadCapacity({
+      minCapacity: 1,
+      maxCapacity: 50,
+    });
+
+    readScaling.scaleOnUtilization({
+      targetUtilizationPercent: 50,
+    });
   }
 }
